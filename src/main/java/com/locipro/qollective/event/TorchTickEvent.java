12a1fb6ceb1b3c83d9b2e67b9444fa9307c12404
lociproMods <abdallahehab309@gmail.com>
2024-11-08 13:54:21 +0300

    Im getting somewhere but idfk, not all loaded chunks work, it's like a weird 3x3 of quarter chunks or smth

--- src/main/java/com/locipro/qollective/event/TorchTickEvent.java
+++ src/main/java/com/locipro/qollective/event/TorchTickEvent.java
@@ -1,57 +1,74 @@
 package com.locipro.qollective.event;
 
+import com.google.common.collect.ImmutableMap;
 import com.locipro.qollective.Qollective;
+import com.locipro.qollective.block.QolBlocks;
 import it.unimi.dsi.fastutil.longs.LongIterator;
-import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.*;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameRules;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.shapes.VoxelShape;
-import net.neoforged.bus.api.Event;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.tick.LevelTickEvent;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
+import java.util.function.Supplier;
 
 @EventBusSubscriber(modid = Qollective.MODID, bus = EventBusSubscriber.Bus.GAME)
 public class TorchTickEvent {
-    public static final int blocksPerChunk = 3;
+    public static final int blocksToChangePerTick = 1;
+
+    private static final ImmutableMap<? extends Block, Supplier<? extends Block>> torch_turnables = ImmutableMap.of(
+            Blocks.TORCH, QolBlocks.UNLIT_TORCH,
+            Blocks.WALL_TORCH, QolBlocks.UNLIT_WALL_TORCH
+    );
+
     @SubscribeEvent
     public static void levelTick(LevelTickEvent.Post event) {
         if (event.getLevel() instanceof ServerLevel level) {
-            if (!level.isRaining()) return;
-            if (level.getRandom().nextBoolean()) return;
+
+            if (level.random.nextFloat() < 0.99) return;
+
+            final int randomTickSpeed = level.getGameRules().getRule(GameRules.RULE_RANDOMTICKING).get();
 
             ChunkMap map = level.getChunkSource().chunkMap;
             LongIterator tickingChunks = map.getDistanceManager().getSpawnCandidateChunks();
 
-            BlockPos[] positions = new BlockPos[blocksPerChunk];
 
+            // for each CHUNK
             tickingChunks.forEachRemaining(aLong -> {
+                List<BlockPos> positions = new ArrayList<>(blocksToChangePerTick);
+
                 ChunkHolder chunk = map.getVisibleChunkIfPresent(aLong);
                 assert chunk != null;
 
-                for (int i = 0; i < level.getGameRules().getRule(GameRules.RULE_RANDOMTICKING).get(); i++) {
+                for (int i = 0; i < randomTickSpeed; i++) {
+                    if (positions.size() >= blocksToChangePerTick) break;
+
                     BlockPos pos = level.getBlockRandomPos(chunk.getPos().x, 0, chunk.getPos().z, 0);
-                    positions[i] = pos;
+
+                    // I dont know how to use height maps
+                    positions.add(new BlockPos(
+                            pos.getX(),
+                            level.getHeight(Heightmap.Types.WORLD_SURFACE, pos.getX(), pos.getZ()) - 1
+                            ,pos.getZ()));
+                }
+                for (BlockPos pos : positions) {
+                    if (pos != null) {
+                        if (!level.isRainingAt(pos.above()) || !level.canSeeSky(pos)) return;
+                        Block blockAtPos = level.getBlockState(pos).getBlock();
+                        if (torch_turnables.containsKey(blockAtPos) && torch_turnables.get(blockAtPos).get() != null) {
+                            BlockState newState = torch_turnables.get(blockAtPos).get().defaultBlockState();
+                            level.setBlock(pos, newState, Block.UPDATE_ALL_IMMEDIATE | Block.UPDATE_SUPPRESS_DROPS);
+                        }
+                    }
                 }
             });
-
-
-
-            for (BlockPos pos : positions) {
-                // Test if it's a torch!
-            }
         }
     }
 
